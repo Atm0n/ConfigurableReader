6a818627f160757e78cd0d769acc67c79e2e9509
nilriera <nilriera2000@gmail.com>
2025-01-19 14:15:10 +0100

    feat: add configurations

---
+++ ConfigurableReader/BookPosition.cs
@@ -0,0 +1,72 @@
+﻿using System.Configuration;
+
+namespace ConfigurableReader;
+
+public class BookPosition : ConfigurationSection
+{
+    [ConfigurationProperty("books")]
+    [ConfigurationCollection(typeof(BookCollection), AddItemName = "book")]
+    public BookCollection Books => (BookCollection)this["books"];
+    public class BookCollection :
+        ConfigurationElementCollection, IEnumerable<Book>
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new Book();
+        }
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((Book)element).Name;
+        }
+        public void Add(Book book)
+        {
+            BaseAdd(book);
+        }
+        public new IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var key in BaseGetAllKeys())
+            {
+                yield return (Book)BaseGet(key);
+            }
+        }
+    }
+    public class Book : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true, IsKey = true)]
+        public string Name
+        {
+            get
+            {
+                return (string)this["name"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+        [ConfigurationProperty("chunk", IsRequired = true)]
+        public int Chunk
+        {
+            get
+            {
+                return (int)this["chunk"];
+            }
+            set
+            {
+                this["chunk"] = value;
+            }
+        }
+        [ConfigurationProperty("scrollPosition", IsRequired = true)]
+        public double ScrollPosition
+        {
+            get
+            {
+                return (double)this["scrollPosition"];
+            }
+            set
+            {
+                this["scrollPosition"] = value;
+            }
+        }
+    }
+}
--- ConfigurableReader/MainWindow.xaml.cs
+++ ConfigurableReader/MainWindow.xaml.cs
@@ -1,25 +1,42 @@
 ﻿using System.IO;
+using System.Configuration;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Xceed.Wpf.Toolkit;
 
 namespace ConfigurableReader;
 
 public partial class MainWindow : Window
 {
     private const string ConfigurableReaderFolder = "ConfigurableReader";
-    private DispatcherTimer _scrollTimer;
+    private readonly DispatcherTimer _scrollTimer;
     private double _scrollSpeed = 0.1;
     private double _currentPosition = 0;
+    private int _currentChunk = 0;
     private string _documentsPath = string.Empty;
-    private string _currentBookFileName = string.Empty;
+    private string? _currentBookFileName;
     private TextChunkReader _chunkReader;
     private IEnumerator<string> _chunkEnumerator;
+    private Configuration configuration;
+    private BookPosition BookPosition;
+    private BookPosition.Book? ActualBook;
 
     public MainWindow()
     {
         InitializeComponent();
+
+        configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+        if (configuration.Sections["bookPositions"] is null)
+        {
+            configuration.Sections.Add("bookPositions", new BookPosition());
+
+        }
+
+        BookPosition = (BookPosition)configuration.GetSection("bookPositions");
+
         _chunkReader = new TextChunkReader();
         _scrollTimer = new()
         {
@@ -30,10 +47,29 @@ public partial class MainWindow : Window
         _documentsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), ConfigurableReaderFolder);
 
         if (!Directory.Exists(_documentsPath))
-        { 
-            // Create the directory
+        {
             Directory.CreateDirectory(_documentsPath);
+
         }
+
+        LoadUserConfiguration();
+
+    }
+
+    private void LoadUserConfiguration()
+    {
+        FontSizeSlider.Value = Properties.Settings.Default.FontSize;
+        ChangeFontSize(Properties.Settings.Default.FontSize);
+
+
+        var colorName = Properties.Settings.Default.TextColor;
+
+        ColorPicker.SelectedColor = Color.FromArgb(colorName.A, colorName.R, colorName.G, colorName.B);
+        AssignNewColorToText();
+
+
+        _scrollSpeed = Properties.Settings.Default.ScrollSpeed;
+        SpeedSlider.Value = Properties.Settings.Default.ScrollSpeed;
     }
 
     private void LoadNextChunk()
@@ -50,17 +86,32 @@ public partial class MainWindow : Window
         {
             _currentBookFileName = openFileDialog.FileName;
 
-            
-            _chunkEnumerator = _chunkReader.ReadChunks(_currentBookFileName, 5024).GetEnumerator();
+            _chunkEnumerator = _chunkReader.ReadChunks(_currentBookFileName, 1024).GetEnumerator(); //5024
 
-            LoadNextChunk();
+            ActualBook = BookPosition.Books.FirstOrDefault(book => book.Name == Path.GetFileName(_currentBookFileName));
 
-            if (File.Exists(Path.Combine(_documentsPath, Path.GetFileName(_currentBookFileName))))
+            if (ActualBook is null)
             {
-                _currentPosition = double.Parse(File.ReadAllText(Path.Combine(_documentsPath, Path.GetFileName(_currentBookFileName))));
-                ScrollViewer.ScrollToHorizontalOffset(_currentPosition);
+
+                ActualBook = new BookPosition.Book()
+                {
+                    Name = Path.GetFileName(_currentBookFileName),
+                    Chunk = 0,
+                    ScrollPosition = 0
+                };
+                BookPosition.Books.Add(ActualBook);
             }
 
+            for (int i = 0; i <= ActualBook.Chunk; i++)
+            {
+                LoadNextChunk();
+
+            }
+
+            _currentPosition = double.Parse(File.ReadAllText(Path.Combine(_documentsPath, Path.GetFileName(_currentBookFileName))));
+            ScrollViewer.ScrollToHorizontalOffset(_currentPosition);
+
+            configuration.Save();
         }
     }
     private void StartButton_Click(object sender, RoutedEventArgs e)
@@ -80,9 +131,10 @@ public partial class MainWindow : Window
         _currentPosition = ScrollViewer.HorizontalOffset;
 
         _currentPosition += _scrollSpeed;
-        if (ScrollViewer.HorizontalOffset == ScrollViewer.ScrollableWidth) 
+        if (ScrollViewer.HorizontalOffset == ScrollViewer.ScrollableWidth)
         {
             LoadNextChunk();
+            _currentChunk++;
             _currentPosition = 0;
         }
 
@@ -92,19 +144,68 @@ public partial class MainWindow : Window
     {
         _scrollSpeed = e.NewValue;
     }
-    private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (ColorComboBox.SelectedItem != null)
-        {
-            ComboBoxItem selectedItem = (ComboBoxItem)ColorComboBox.SelectedItem;
-            string colorName = selectedItem.Content.ToString();
-            TextBlock.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorName));
-        }
-    }
     private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
     {
-        if (TextBlock is not null)
-            TextBlock.FontSize = e.NewValue;
+        ChangeFontSize(e.NewValue);
     }
 
+    private void ChangeFontSize(double value)
+    {
+        if (TextBlock is not null)
+            TextBlock.FontSize = value;
+    }
+
+    private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+    {
+
+        AssignNewColorToText();
+    }
+
+    private void AssignNewColorToText()
+    {
+        if (ColorPicker.SelectedColor is not null)
+        {
+            TextBlock.Foreground = new SolidColorBrush((Color)ColorPicker.SelectedColor);
+        }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        SaveActualBookConfiguration();
+        SaveUserConfiguration();
+
+        configuration.Save();
+
+    }
+
+    private void SaveActualBookConfiguration()
+    {
+        if (ActualBook is not null)
+        {
+            ActualBook.Chunk = _currentChunk;
+            ActualBook.ScrollPosition = _currentPosition;
+
+        }
+    }
+
+    private void SaveUserConfiguration()
+    {
+        Properties.Settings.Default.FontSize = FontSizeSlider.Value;
+
+        if (ColorPicker.SelectedColor is not null)
+        {
+            var color = ColorPicker.SelectedColor.Value;
+            Properties.Settings.Default.TextColor = System.Drawing.Color.FromArgb
+            (
+                color.A,
+                color.R,
+                color.G,
+                color.B
+            );
+        }
+
+        Properties.Settings.Default.ScrollSpeed = _scrollSpeed;
+
+        Properties.Settings.Default.Save();
+    }
 }
